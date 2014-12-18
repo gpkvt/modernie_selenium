f565ca96dcab3fd9919ad4930c8f26df0df7170f
Toby Bettridge <toby.bettridge@stubside.com>
2014-12-18 23:41:18 +0000

    Snapshot. Need a hub. See http://stackoverflow.com/questions/13241412/selenium-node-failed-to-register-with-the-hub

--- Tools/selenium_conf/selenium.bat
+++ Tools/selenium_conf/selenium.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Java\jre7\bin\java.exe" -jar c:\selenium\selenium-server-standalone-2.41.0.jar -role node -nodeConfig c:\selenium\config.json
+"C:\Program Files\Java\jre1.8.0_25\bin\java.exe" -jar c:\selenium\selenium-server-standalone-2.44.0.jar -role node -nodeConfig c:\selenium\config.json
