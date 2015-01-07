ad7bc6089b939423f48e682b6e48ccc5af8ec507
Denis Witt <gpkvt@users.noreply.github.com>
2015-01-07 23:44:37 +0100

    Delete test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.Remote("http://192.168.1.23:5555/wd/hub", webdriver.DesiredCapabilities.INTERNETEXPLORER.copy())
-driver.get("http://www.google.com")
-driver.get_screenshot_as_file('/Screenshots/google.png')
