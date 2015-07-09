96c2b3ef58b9f21508282e00edc44ef37fd9fac6
Juan Garcia <dev@soleilnoirmedia.com>
2015-07-08 23:13:05 -0400

    Added Chrome and ChromeDriver installation. Updated Java installation. Added gitignore file.

--- Tools/selenium_conf/selenium.bat
+++ Tools/selenium_conf/selenium.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Java\jre1.8.0_25\bin\java.exe" -jar c:\selenium\selenium-server-standalone.jar -role node -nodeConfig c:\selenium\config.json
+"C:\Program Files\Java\jre7\bin\java.exe" -jar c:\selenium\selenium-server-standalone.jar -role node -nodeConfig c:\selenium\config.json
